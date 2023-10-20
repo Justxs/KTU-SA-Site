f22e5d5b0ab570390284e2fc61ecc6d68b15d066
Justas Pranauskis <justas.pranauskis@ktu.edu>
2023-10-20 03:13:15 +0300

    other: image cards on hero image

--- KTU-SA-WEB/src/pages/home/Components/HeroImage.jsx
+++ KTU-SA-WEB/src/pages/home/Components/HeroImage.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import styles from "./HeroImage.module.css";
+import img1 from "../../../assets/palceholder2.png";
+import img2 from "../../../assets/KtuSA-Flag.png";
+import img3 from "../../../assets/placeholder.png";
 
 function HeroImage() {
   return (
@@ -10,6 +13,23 @@ function HeroImage() {
           Organizacija siekanti vieno tikslo, ginti visų studentų interesus,
           sprendžiant šiandienos ir rytojaus iššūkius
         </p>
+      </div>
+      <div className={styles.Images}>
+        <div className={styles.ImagePos1}>
+          <div className={styles.ImageCard1}>
+            <img src={img1} className={styles.Image} />
+          </div>
+        </div>
+        <div className={styles.ImagePos2}>
+          <div className={styles.ImageCard2}>
+            <img src={img2} className={styles.Image} />
+          </div>
+        </div>
+        <div className={styles.ImagePos3}>
+          <div className={styles.ImageCard3}>
+            <img src={img3} className={styles.Image} />
+          </div>
+        </div>
       </div>
     </div>
   );
