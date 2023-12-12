a5698ea0de7ae124275a7359d9c1038265b9ca17
Justas Pranauskis <justas.pranauskis@ktu.edu>
2023-12-12 02:39:30 +0200

    Feat: added FSA section

---
+++ KTU-SA-WEB/src/constants/FsaUnits.js
@@ -0,0 +1,62 @@
+import ESA from "../assets/FsaLogo/ESA.png"
+import FUMSA from "../assets/FsaLogo/FUMSA.png"
+import INDI from "../assets/FsaLogo/INDI.png"
+import INFOSA from "../assets/FsaLogo/INFOSA.png"
+import SHM from "../assets/FsaLogo/SHM.png"
+import STATIUS from "../assets/FsaLogo/STATIUS.png"
+import VFSA from "../assets/FsaLogo/VFSA.png"
+import VIVAT from "../assets/FsaLogo/VIVAT.png"
+
+
+const FSA_DATA = [
+    {
+      name: "FSA VIVAT chemija",
+      fullName: "Cheminės techologijos fakulteto studentų atstovybė „VIVAT chemija“",
+      description: "A student organization focused on chemical technology studies, promoting academic and social activities among its members.",
+      logo: VIVAT,
+    },
+    {
+      name: "FSA FUMSA",
+      fullName: "Matematikos ir gamtos mokslų fakulteto studentų atstovybė FUMSA",
+      description: "Dedicated to representing students from the Faculty of Mathematics and Natural Sciences, FUMSA organizes educational and recreational events.",
+      logo: FUMSA,
+    },
+    {
+      name: "FSA InDi",
+      fullName: "Mechanikos inžinerijos ir dizaino fakulteto atstovybė InDi",
+      description: "InDi represents students of Mechanical Engineering and Design, fostering a community of innovation and creativity.",
+      logo: INDI,
+    },
+    {
+      name: "FSA SHM",
+      fullName: "Socialinių, humanitarinių mokslų ir menų fakulteto studentų atstovybė SHM",
+      description: "Bringing together students from social sciences, humanities, and arts to engage in multidisciplinary activities and advocacy.",
+      logo: SHM,
+    },
+    {
+      name: "FSA STATIUS",
+      fullName: "Statybos ir architetūros fakulteto studentų atstovybė STATIUS",
+      description: "STATIUS is the voice of students in the Faculty of Construction and Architecture, organizing events and promoting professional development.",
+      logo: STATIUS,
+    },
+    {
+      name: "FSA VFSA",
+      fullName: "Ekonomikos ir verslo fakulteto studentų atstovybė VFSA",
+      description: "A hub for students in Economics and Business, VFSA arranges seminars, workshops, and networking opportunities.",
+      logo: VFSA,
+    },
+    {
+      name: "FSA ESA",
+      fullName: "Elektros ir elektronikos fakulteto studentų atstovybė ESA",
+      description: "ESA represents Electrical and Electronics Engineering students, focusing on technical skills and industry connections.",
+      logo: ESA,  
+    },
+    {
+      name: "FSA InfoSA",
+      fullName: "Informatikos fakulteto studentų atstovybė InfoSA",
+      description: "InfoSA connects students of the Faculty of Informatics, organizing tech talks, coding challenges, and social gatherings.",
+      logo: INFOSA,
+    }
+  ];
+  
+export default FSA_DATA;
--- KTU-SA-WEB/src/pages/home/Components/FSA/Fsa.jsx
+++ KTU-SA-WEB/src/pages/home/Components/FSA/Fsa.jsx
@@ -1,17 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import SectionName from "../../../../components/sectionName/SectionName";
 import styles from "./Fsa.module.css";
+import KTU_SA_Logo from "../../../../assets/KTU_SA_Logo.png";
+import { Button, styled } from "@mui/material";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import FSA_DATA from "../../../../constants/FsaUnits";
+import { motion } from "framer-motion";
 
-function Fsa() {
+const FsaButton = styled(Button)({
+  color: "#0E2643",
+  background: "#fff",
+  textTransform: "none",
+  padding: "12px",
+  fontSize: "20px",
+  "&:hover": {
+    background: "#fff",
+    color: "#4A9FE6",
+  },
+});
+
+export default function Fsa() {
+  const [currentLogo, setCurrentLogo] = useState(KTU_SA_Logo);
+
+  const logoVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+  };
+
   return (
     <>
       <SectionName
         title="Raskite savo fakulteto studentų atstovybę"
         showArrow={false}
       />
-      <div className={styles.Container}></div>
+      <div className={styles.Container}>
+        <div className={styles.LogoContainer}>
+          <motion.img
+            key={currentLogo}
+            src={currentLogo}
+            alt="FSA Logo"
+            className={styles.Logo}
+            variants={logoVariants}
+            initial="hidden"
+            animate="visible"
+            exit="hidden"
+            transition={{ duration: 0.5 }}
+          />
+        </div>
+        <div className={styles.FsaList}>
+          {FSA_DATA.map((fsa) => (
+            <div
+              key={fsa.name}
+              onMouseEnter={() => setCurrentLogo(fsa.logo)}
+              onMouseLeave={() => setCurrentLogo(KTU_SA_Logo)}
+            >
+              <FsaButton>
+                {fsa.fullName}
+                <ArrowForwardIcon className={styles.Arrow} />
+              </FsaButton>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
-
-export default Fsa;
