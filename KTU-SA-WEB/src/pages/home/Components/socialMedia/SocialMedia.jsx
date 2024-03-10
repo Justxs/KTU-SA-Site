bae6eedc33365f036d79165bc36ac65076d5a795
Justas Pranauskis <justas.pranauskis@ktu.edu>
2024-03-11 00:45:02 +0200

    Feat: social links are now with pictures

--- KTU-SA-WEB/src/pages/home/Components/socialMedia/SocialMedia.jsx
+++ KTU-SA-WEB/src/pages/home/Components/socialMedia/SocialMedia.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import styles from "./SocialMedia.module.css";
-import InstagramIcon from "../../../../assets/Instagram-colored.svg";
-import FacebookIcon from "../../../../assets/icon-facebook.svg";
-import LinkedinIcon from "../../../../assets/icon-linkedin.svg";
-import { SOCIAL_LINKS } from "../../../../constants/socialLinks";
+import SocialLinks from "./components/SocialLinks";
+import PlaceHolder1 from "../../../../assets/placeholder.png";
+import PlaceHolder2 from "../../../../assets/placeholder2.png";
+import PlaceHolder3 from "../../../../assets/placeholder3.jpg";
+import PlaceHolder4 from "../../../../assets/placeholder4.jpg";
+import DepthSvg from "../../../../assets/playfullImages/Depth.svg";
+import SmileySvg from "../../../../assets/playfullImages/Smiley.svg";
+import InfinitySvg from "../../../../assets/playfullImages/Infinity.svg";
 
 export default function SocialMedia() {
   return(
@@ -14,31 +18,15 @@ export default function SocialMedia() {
           <div className={styles.Text}>
             SEKITE MUS SOCIALINIUOSE TINKLUOSE
           </div>
-          <div className={styles.IconsContainer}>
-            <a
-              href={SOCIAL_LINKS.INSTAGRAM}
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.Icon}
-            >
-              <img src={InstagramIcon} width={95} />
-            </a>
-            <a
-              href={SOCIAL_LINKS.LINKEDIN}
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.Icon}
-            >
-              <img src={LinkedinIcon} width={100} />
-            </a>
-            <a
-              href={SOCIAL_LINKS.FACEBOOK}
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.Icon}
-            >
-              <img src={FacebookIcon} width={100} />
-            </a>
+          <SocialLinks />
+          <div className={styles.ImagesContainer}>
+            <img className={styles.Image} src={PlaceHolder1} />
+            <img className={styles.Image} src={PlaceHolder2} />
+            <img className={styles.Image} src={PlaceHolder3} />
+            <img className={styles.Image} src={PlaceHolder4} />
+            <img className={styles.Icon} src={DepthSvg}/>
+            <img className={styles.Icon} src={SmileySvg}/>
+            <img className={styles.Icon} src={InfinitySvg}/>
           </div>
         </div>
       </div>
---
+++ KTU-SA-WEB/src/pages/home/Components/socialMedia/components/SocialLinks.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { SOCIAL_LINKS } from '../../../../../constants/socialLinks';
+import InstagramIcon from "../../../../../assets/Instagram-colored.svg";
+import FacebookIcon from "../../../../../assets/icon-facebook.svg";
+import LinkedinIcon from "../../../../../assets/icon-linkedin.svg";
+import styles from "./SocialLinks.module.css";
+
+export default function SocialLinks() {
+  return (
+    <div className={styles.IconsContainer}>
+      <a
+        href={SOCIAL_LINKS.INSTAGRAM}
+        target="_blank"
+        rel="noopener noreferrer"
+        className={styles.Icon}
+      >
+        <img src={InstagramIcon} width={92} alt='Instagram' />
+      </a>
+      <a
+        href={SOCIAL_LINKS.LINKEDIN}
+        target="_blank"
+        rel="noopener noreferrer"
+        className={styles.Icon}
+      >
+        <img src={LinkedinIcon} width={100} alt='Linkedin' />
+      </a>
+      <a
+        href={SOCIAL_LINKS.FACEBOOK}
+        target="_blank"
+        rel="noopener noreferrer"
+        className={styles.Icon}
+      >
+        <img src={FacebookIcon} width={100} alt='Facebook' />
+      </a>
+    </div>
+  );
+}
