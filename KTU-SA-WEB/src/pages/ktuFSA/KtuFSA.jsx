aa496d64adbdf51b52e6c2ec92d97df1d5e16eb4
Justas Pranauskis <justas.pranauskis@ktu.edu>
2024-03-18 02:54:46 +0200

    fix: deployment error

--- KTU-SA-WEB/src/pages/ktuFSA/KtuFSA.jsx
+++ KTU-SA-WEB/src/pages/ktuFSA/KtuFSA.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import HeroImage from "./components/heroImage/HeroImage";
 import styles from "./KtuFSA.module.css";
-import FallbackWrapper from "../../components/fallbackWrapper/FallbackWrapper";
 import ContactCard from "../../components/contactCard/ContactCard";
 import placeholder from "../../assets/male-avatar-placeholder.png";
 import SectionName from "../../components/sectionName/SectionName";
@@ -10,7 +9,7 @@ import SectionName from "../../components/sectionName/SectionName";
 function KtuFSA() {
   const { fsaName } = useParams();
 
-  const { data: contacts, isLoading } = {};
+  const { data: contacts } = {};
 
   return (
     <div className={styles.Container}>
@@ -18,21 +17,18 @@ function KtuFSA() {
       <div className={styles.Container}>
         <SectionName title="Komanda" showArrow/>
         <div className={styles.ContactCards}>
-          <FallbackWrapper isLoading={isLoading} data={contacts}>
-            {contacts && contacts
-              .filter(contact => contact.fullName !== null)
-              .map(contact => (
-                <ContactCard
-                  key={contact.id}
-                  name={contact.fullName}
-                  position={contact.positionName}
-                  email={contact.email}
-                  phone={contact.phoneNumber}
-                  photo={placeholder}
-                />
-              ))
-            }
-          </FallbackWrapper>
+          {contacts && contacts
+            .map(contact => (
+              <ContactCard
+                key={contact.id}
+                name={contact.fullName}
+                position={contact.positionName}
+                email={contact.email}
+                phone={contact.phoneNumber}
+                photo={placeholder}
+              />
+            ))
+          }
         </div>
       </div>
     </div>
