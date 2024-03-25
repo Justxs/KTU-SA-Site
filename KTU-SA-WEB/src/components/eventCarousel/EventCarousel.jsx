0c816f6718b38de1db2c6370e316484519ec92ed
Justas Pranauskis <justas.pranauskis@ktu.edu>
2024-03-25 03:59:39 +0200

    Feat: event carousel

---
+++ KTU-SA-WEB/src/components/eventCarousel/EventCarousel.jsx
@@ -0,0 +1,139 @@
+import React from 'react';
+import styles from "./EventCarousel.module.css";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { ArrowLeft, ArrowRight } from '@mui/icons-material';
+
+const slides = [
+  {
+    title: 'Renginio pavadinimas 1',
+    date: '2023-10-10 18:00',
+    imgPath: 'https://source.unsplash.com/random/1',
+  },
+  {
+    title: 'Renginio pavadinimas 2',
+    date: '2023-10-10 18:00',
+    imgPath: 'https://source.unsplash.com/random/2',
+  },
+  {
+    title: 'Renginio pavadinimas 3',
+    date: '2023-10-10 18:00',
+    imgPath: 'https://source.unsplash.com/random/3',
+  },
+  {
+    title: 'Renginio pavadinimas 4',
+    date: '2023-10-10 18:00',
+    imgPath: 'https://source.unsplash.com/random/4',
+  },
+  {
+    title: 'Renginio pavadinimas 5',
+    date: '2023-10-10 18:00',
+    imgPath: 'https://source.unsplash.com/random/5',
+  },
+  {
+    title: 'Renginio pavadinimas 6',
+    date: '2023-10-10 18:00',
+    imgPath: 'https://source.unsplash.com/random/6',
+  },
+];
+
+function SampleNextArrow(props) {
+  // eslint-disable-next-line react/prop-types
+  const { className, style, onClick } = props;
+
+  return (
+    <div
+      className={className}
+      style={{ ...style}}
+      onClick={onClick}
+    >
+      <div className={styles.ArrowRight}>
+        <ArrowRight style={{ fontSize: '100px' }}/>
+      </div>
+    </div>
+  );
+}
+  
+function SamplePrevArrow(props) {
+  // eslint-disable-next-line react/prop-types
+  const { className, style, onClick } = props;
+
+  return (
+    <div
+      className={className}
+      style={{ ...style}}
+      onClick={onClick}
+    >
+      <div className={styles.ArrowLeft}>
+        <ArrowLeft style={{ fontSize: '100px' }}/>
+      </div>
+    </div>
+  );
+}
+  
+export default function EventCarousel() {
+  var settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: <SamplePrevArrow to="prev"/>,
+    nextArrow: <SampleNextArrow to="next"/>,
+    responsive: [
+      {
+        breakpoint: 1900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 1350,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          autoplay: true,
+          infinite: true,
+          prevArrow: <></>,
+          nextArrow: <></>,
+        }
+      }
+    ]
+  };
+
+  return (
+    <div className={styles.Container}>
+      <Slider {...settings}>
+        {slides.map((slide, index) => (
+          <div key={index} >
+            <div className={styles.CardContainer}>
+              <div className={styles.Card}>
+                <img 
+                  src={slide.imgPath} 
+                  alt={slide.title} 
+                  className={styles.Image}
+                />
+                <b className={styles.Title}>
+                  {slide.title}
+                </b>
+                <div className={styles.Date}>
+                  {slide.date}
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+}
--- KTU-SA-WEB/src/pages/home/Components/events/Events.jsx
+++ KTU-SA-WEB/src/pages/home/Components/events/Events.jsx
@@ -3,17 +3,19 @@ import SectionName from "../../../../components/sectionName/SectionName";
 import ReadMoreButton from "../../../../components/readMoreButton/ReadMoreButton";
 import styles from "./EventsCarousel.module.css";
 import { useTranslation } from "react-i18next";
+import EventCarousel from "../../../../components/eventCarousel/EventCarousel";
 
-function EventsCarousel() {
+export default function Events() {
   const { t } = useTranslation();
   return (
     <>
       <SectionName title={t('sections.events')} showArrow />
+      <div className={styles.Container}>
+        <EventCarousel />
+      </div>
       <div className={styles.ButtonContainer}>
         <ReadMoreButton title={t('button.events')} path="/Events" />
       </div>
     </>
   );
 }
-
-export default EventsCarousel;
