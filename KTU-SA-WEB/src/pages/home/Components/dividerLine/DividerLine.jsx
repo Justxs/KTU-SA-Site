03d20a5db7e856db73c94ed665e51b1eb0feba53
Justas Pranauskis <justas.pranauskis@ktu.edu>
2024-04-06 08:00:19 +0300

    fix: fixed margins for divider line

--- KTU-SA-WEB/src/pages/home/Components/dividerLine/DividerLine.jsx
+++ KTU-SA-WEB/src/pages/home/Components/dividerLine/DividerLine.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { motion } from 'framer-motion';
 import { useTranslation } from 'react-i18next';
 import styles from './DividerLine.module.css';
+import AbsoluteContainerMargin from '../../../../components/marginContainers/ObsoluteContainerMargin';
 
 export default function DividerLine() {
   const { t } = useTranslation();
   const text = t('home.dividerLine');
+  const elementRef = useRef(null);
 
   const scrollTextAnimation = {
     animate: {
@@ -19,16 +21,18 @@ export default function DividerLine() {
   };
 
   return (
-    <div className={styles.Container}>
-      <div className={styles.Divider}>
-        <motion.span
-          className={styles.Text}
-          variants={scrollTextAnimation}
-          animate="animate"
-        >
-          {text}
-        </motion.span>
+    <AbsoluteContainerMargin elementRef={elementRef}>
+      <div className={styles.Container} ref={elementRef}>
+        <div className={styles.Divider}>
+          <motion.span
+            className={styles.Text}
+            variants={scrollTextAnimation}
+            animate="animate"
+          >
+            {text}
+          </motion.span>
+        </div>
       </div>
-    </div>
+    </AbsoluteContainerMargin>
   );
 }
