4c0edfef2a8a761c5139871f3fb2e64520d08474
Justas Pranauskis <justas.pranauskis@presentconnection.eu>
2024-07-04 21:38:56 +0300

    refactor: moved images to cloud

---
+++ KTU-SA-WEB/src/constants/photos.js
@@ -0,0 +1,6 @@
+export const PHOTOS = {
+  SHM_GIRLS_PHOTO_URL: 'https://storage.googleapis.com/ktusawebsite/placeholder2.jpg',
+  KTU_SA_PRESIDENT_PHOTO_URL: 'https://storage.googleapis.com/ktusawebsite/placeholder3.jpg',
+  DISCUSSION_PHOTO_URL: 'https://storage.googleapis.com/ktusawebsite/placeholder.png',
+  KTU_SA_PHOTO_URL: 'https://storage.googleapis.com/ktusawebsite/placeholder4.jpg',
+};
--- KTU-SA-WEB/src/pages/home/Components/socialMedia/SocialMedia.jsx
+++ KTU-SA-WEB/src/pages/home/Components/socialMedia/SocialMedia.jsx
@@ -2,14 +2,11 @@ import React, { useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import styles from './SocialMedia.module.css';
 import SocialLinks from './components/SocialLinks';
-import PlaceHolder1 from '../../../../assets/placeholder.png';
-import PlaceHolder2 from '../../../../assets/placeholder2.png';
-import PlaceHolder3 from '../../../../assets/placeholder3.jpg';
-import PlaceHolder4 from '../../../../assets/placeholder4.jpg';
 import DepthSvg from '../../../../assets/playfullImages/Depth.svg';
 import SmileySvg from '../../../../assets/playfullImages/Smiley.svg';
 import InfinitySvg from '../../../../assets/playfullImages/Infinity.svg';
 import AbsoluteContainerMargin from '../../../../components/marginContainers/ObsoluteContainerMargin';
+import { PHOTOS } from '../../../../constants/photos';
 
 export default function SocialMedia() {
   const { t } = useTranslation();
@@ -26,10 +23,10 @@ export default function SocialMedia() {
             </div>
             <SocialLinks />
             <div className={styles.ImagesContainer}>
-              <img className={styles.Image} src={PlaceHolder1} alt="" />
-              <img className={styles.Image} src={PlaceHolder2} alt="" />
-              <img className={styles.Image} src={PlaceHolder3} alt="" />
-              <img className={styles.Image} src={PlaceHolder4} alt="" />
+              <img className={styles.Image} src={PHOTOS.DISCUSSION_PHOTO_URL} alt="" />
+              <img className={styles.Image} src={PHOTOS.SHM_GIRLS_PHOTO_URL} alt="KTU FSA SHM girls" />
+              <img className={styles.Image} src={PHOTOS.KTU_SA_PRESIDENT_PHOTO_URL} alt="" />
+              <img className={styles.Image} src={PHOTOS.KTU_SA_PHOTO_URL} alt="" />
               <img className={styles.Icon} src={DepthSvg} alt="" />
               <img className={styles.Icon} src={SmileySvg} alt="" />
               <img className={styles.Icon} src={InfinitySvg} alt="" />
